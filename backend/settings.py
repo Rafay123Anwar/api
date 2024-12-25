0f733f87c9b7a10b3a09d2447304a2e4ad08eb74
[Rafay123Anwar] <[muhammadrafayy0@gmail.com]>
2024-12-26 03:15:33 +0500

    first commit

--- backend/settings.py
+++ backend/settings.py
@@ -61,6 +61,11 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    # Add other domains if needed
+]
+
 
 ROOT_URLCONF = 'backend.urls'
 
